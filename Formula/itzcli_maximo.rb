fe3c9e9584f75f30085065f4d7ae4b01532cbc98
lfDev28 <lee.forster28@gmail.com>
2024-04-29 14:52:58 +1000

    Changes

--- Formula/itzcli_maximo.rb
+++ Formula/itzcli_maximo.rb
@@ -1,7 +1,7 @@
 class ItzcliMaximo < Formula
     desc "CLI for Techzone Provisioning - Maximo Team customization of root itzcli"
     homepage "https://github.com/lfDev28/itzcli"
-    url "https://github.com/lfDev28/itzcli/archive/v.1.0.0-alpha.tar.gz"
+    url "https://github.com/lfDev28/itzcli/archive/v0.1.0.tar.gz"
     sha256 "cab38d4557359ffe8218899da6a217f51be1939d2c54ecb099d21174709d46c6"
     license "MIT"
 
