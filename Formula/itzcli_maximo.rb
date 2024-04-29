c6361e1e85793b6b32cc4cee063176c76799e3f1
lfDev28 <lee.forster28@gmail.com>
2024-04-29 15:33:36 +1000

    Changes

--- Formula/itzcli_maximo.rb
+++ Formula/itzcli_maximo.rb
@@ -1,10 +1,10 @@
 class ItzcliMaximo < Formula
     desc "CLI for Techzone Provisioning - Maximo Team customization of root itzcli"
     homepage "https://github.com/lfDev28/itzcli"
-    url "https://github.com/lfDev28/itzcli/archive/itzcli.tar.gz"
+    url "https://github.com/lfDev28/itzcli/archive/v0.1.0.tar.gz"
     sha256 "867a4a816580a59c71cce7edc6978ded7dd66d9b230ebf2aec848a4a84173eb4"
     license "MIT"
-    version "0.1.0"
+    version "v0.1.0"
 
     depends_on "go"
 
@@ -13,7 +13,7 @@ class ItzcliMaximo < Formula
         system "make", "install", "ITZ_VER=#{version}"
 
         # Move the binary to the bin folder
-        bin.install "itz"
+        bin.install "itzcli"
 
     end
 
