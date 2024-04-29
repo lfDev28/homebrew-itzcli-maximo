3e44b237397c681e8b24271a418519e74926e2e1
lfDev28 <lee.forster28@gmail.com>
2024-04-29 15:11:44 +1000

    Changes

--- Formula/itzcli_maximo.rb
+++ Formula/itzcli_maximo.rb
@@ -5,15 +5,12 @@ class ItzcliMaximo < Formula
     sha256 "867a4a816580a59c71cce7edc6978ded7dd66d9b230ebf2aec848a4a84173eb4"
     license "MIT"
 
+    depends_on "go"
+
     def install 
         # Run the make files install command
-        system "make", "install", "PREFIX=#{prefix}"
+        system "make", "install"
 
-        # Rename the binary to itz
-        mv "#{bin}/itzcli", "#{bin}/itz"
-
-        # Create a symlink to the binary
-        bin.install_symlink "#{bin}/itz"
 
     end
 
