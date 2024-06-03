68b44a169b02094d51a1b1c47c56a974d7d1f54c
lfDev28 <116426164+lfDev28@users.noreply.github.com>
2024-06-03 15:43:44 +1000

    Update itzcli_maximo.rb

--- Formula/itzcli_maximo.rb
+++ Formula/itzcli_maximo.rb
@@ -4,7 +4,7 @@ class ItzcliMaximo < Formula
     url "https://github.com/lfDev28/itzcli/archive/v.0.1.33.tar.gz"
     sha256 "17f22ba735b6dd9ff1147840a22b89c92f7cbe329edbadc4b13580c883b5e14f"
     license "MIT"
-    version "v.0.1.32"
+    version "v.0.1.33"
 
     depends_on "go"
 
