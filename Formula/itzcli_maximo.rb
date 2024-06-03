c532a33c69f4cb4994d6a84d50fa02837497f70b
lfDev28 <116426164+lfDev28@users.noreply.github.com>
2024-06-03 15:22:21 +1000

    Update itzcli_maximo.rb

--- Formula/itzcli_maximo.rb
+++ Formula/itzcli_maximo.rb
@@ -1,7 +1,7 @@
 class ItzcliMaximo < Formula
     desc "CLI for Techzone Provisioning - Maximo Team customization of root itzcli"
     homepage "https://github.com/lfDev28/itzcli"
-    url "https://github.com/lfDev28/itzcli/archive/v.0.1.32.tar.gz"
+    url "https://github.com/lfDev28/itzcli/archive/v.0.1.33.tar.gz"
     sha256 "96a7dd18e1331c934757bb4bc6be8d0b24a3a81a971b05d51fd14ce16b6e08ea"
     license "MIT"
     version "v.0.1.32"
